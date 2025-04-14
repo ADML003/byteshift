3d8e46ce1373b9035231d28c4f1016febdf8b327
Aditya Malhotra <adml003.pro@gmail.com>
2025-04-14 19:05:27 +0530

    social share set up

---
+++ app/(app)/social-share/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SocialShare = () => {
+  return (
+    <div>
+      <p>welcome to the social share</p>
+      SocialShare
+    </div>
+  );
+};
+
+export default SocialShare;
